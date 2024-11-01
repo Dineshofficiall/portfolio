10d30ab1b7b92c642fe1e9d4b30e4a0facb81f6a
Dinesh Kumar <dineshsaravananofficial@gmail.com>
2024-11-01 14:55:22 +0530

    base file routing name changed

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/portfolio/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
